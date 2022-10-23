aade2c2e7cf9319014f0a9babaacbd9c9e3f5082
“T3sultan” <incognito.soul011@gmail.com>
2022-10-23 23:44:12 +0600

    appointment doctor

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "./Banner";
 import Exceptional from "./Exceptional";
 import Information from "./Information";
+import MakeAppointment from "./MakeAppointment";
 import Services from "./Services";
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
       <Information />
       <Services />
       <Exceptional />
+      <MakeAppointment />
     </div>
   );
 };
