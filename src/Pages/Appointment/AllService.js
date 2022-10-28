fc0629b825e15108a0c351aded1591810533b1fe
“T3sultan” <incognito.soul011@gmail.com>
2022-10-28 23:13:22 +0600

    dynamic modal

--- src/Pages/Appointment/AllService.js
+++ src/Pages/Appointment/AllService.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const AllService = ({ service }) => {
+const AllService = ({ service, setTreatment }) => {
   const { name, slots } = service;
   return (
     <div className="card lg:max-w-lg bg-base-100 shadow-xl">
@@ -19,12 +19,15 @@ const AllService = ({ service }) => {
           {slots.length} {slots.length > 1 ? "spaces" : "space"} available
         </p>
         <div className="card-actions justify-center">
-          <button
+          {/* <button className=""></button> */}
+          <label
+            onClick={() => setTreatment(service)}
             disabled={slots.length === 0}
+            htmlFor="booking-modal"
             className="btn btn-secondary text-white uppercase"
           >
             Book Appointment
-          </button>
+          </label>
         </div>
       </div>
     </div>
--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,9 +1,13 @@
 import { format } from "date-fns";
 import React, { useEffect, useState } from "react";
 import AllService from "./AllService";
+import BookingModal from "./BookingModal";
 
 const AvailableAppointment = ({ date }) => {
   const [services, setServices] = useState([]);
+
+  const [treatment, setTreatment] = useState(null);
+
   useEffect(() => {
     fetch("services.json")
       .then(res => res.json())
@@ -16,9 +20,14 @@ const AvailableAppointment = ({ date }) => {
       </h2>
       <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
         {services.map(service => (
-          <AllService key={service._id} service={service} />
+          <AllService
+            setTreatment={setTreatment}
+            key={service._id}
+            service={service}
+          />
         ))}
       </div>
+      {treatment && <BookingModal treatment={treatment} />}
     </div>
   );
 };
