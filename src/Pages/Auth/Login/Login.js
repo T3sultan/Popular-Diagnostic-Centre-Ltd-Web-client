280e392325d02c6cd88e6fddebd1cbf279fe561e
“T3sultan” <incognito.soul011@gmail.com>
2022-10-30 23:10:22 +0600

    login from ui

--- src/Pages/Auth/Login/Login.js
+++ src/Pages/Auth/Login/Login.js
@@ -1,7 +1,17 @@
 import React from "react";
 import auth from "../../../firebase.init";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useForm } from "react-hook-form";
+
 const Login = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = data => console.log(data);
+
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
   if (user) {
     console.log(user);
@@ -11,6 +21,72 @@ const Login = () => {
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className=" text-center text-2xl font-bold">Login</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Your Email"
+                className="input input-bordered w-full max-w-xs"
+                {...register("email", {
+                  required: { value: true, message: "Email is required" },
+                  pattern: {
+                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                    message: "error message", // JS only: <p>error message</p> TS only support string
+                  },
+                })}
+              />
+              <label className="label">
+                {errors.email?.type === "required" && (
+                  <p className="text-red-400" role="alert">
+                    {errors.email.message}
+                  </p>
+                )}
+                {errors.email?.type === "pattern" && (
+                  <p className="text-red-400" role="alert">
+                    {errors.email.message}
+                  </p>
+                )}
+              </label>
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Your Password"
+                className="input input-bordered w-full max-w-xs"
+                {...register("password", {
+                  required: { value: true, message: "Password is required" },
+                  minLength: {
+                    value: 6,
+                    message: "Must be 6 characters or longer", // JS only: <p>error message</p> TS only support string
+                  },
+                })}
+              />
+              <label className="label">
+                {errors.password?.type === "required" && (
+                  <p className="text-red-400" role="alert">
+                    {errors.password.message}
+                  </p>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <p className="text-red-400" role="alert">
+                    {errors.password.message}
+                  </p>
+                )}
+              </label>
+            </div>
+
+            <input
+              className="btn w-full max-w-xs text-black"
+              type="submit"
+              value="Login"
+            />
+          </form>
           <div className="divider">OR</div>
           <button
             onClick={() => signInWithGoogle()}
